22d7279cb1ec615cfa555bd17b9388ecdae916ba
fadeynotchenko <notchenkofaddey@gmail.com>
2022-07-17 16:04:19 +0300

    Initial Commit

---
+++ GoalApp/GoalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalAppApp.swift
+//  GoalApp
+//
+//  Created by Fadey Notchenko on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct GoalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
